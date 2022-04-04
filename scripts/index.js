d25142783c2dca835f986e8e2f6dfbba73c4ad16
Salomão Santos <salomaosantos3949@gmail.com>
2022-04-04 20:52:11 -0300

    updated with method POST

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,7 @@
 let form = document.querySelector('form')
 let emailReference = document.querySelector('#inputEmail')
 let passwordReference = document.querySelector('#inputPassword')
+let buttonLogin = document.querySelector('#buttonLogin')
 let ulErrorList = document.querySelector('.error ul')
 let errorList = document.querySelector('.error')
 
@@ -12,7 +13,7 @@ const errorMessage = message => {
   ulErrorList.innerHTML += `<li>${message}</li>`
 }
 
-form.addEventListener('submit', event => {
+form.addEventListener('click', event => {
   ulErrorList.innerHTML = ''
 
   if (empty(emailReference)) {
@@ -26,4 +27,33 @@ form.addEventListener('submit', event => {
     event.preventDefault()
     errorList.hidden = ''
   }
+})
+
+buttonLogin.addEventListener('click', event => {
+  event.preventDefault()
+
+  let credencials = {
+    email: 'string',
+    password: 'string'
+  }
+
+  let headerRequest = {
+    'Content-Type': 'application/json'
+  }
+
+  let resquestConfig = {
+    method: 'POST',
+    body: JSON.stringify(credencials),
+    headers: headerRequest
+  }
+
+  fetch(
+    'https://ctd-todo-api.herokuapp.com/v1/users/login',
+    resquestConfig
+  ).then(response => {
+    response.json().then(data => {
+      console.log(data.jwt)
+      localStorage.setItem('token', data.jwt)
+    })
+  })
 })
--- scripts/singup.js
+++ scripts/singup.js
@@ -2,6 +2,8 @@ let inputNameReference = document.querySelector('#inputName')
 let inputSurnameReference = document.querySelector('#inputSurname')
 let inputEmailReference = document.querySelector('#inputEmail')
 let inputPasswordReference = document.querySelector('#inputPassword')
+let inputPasswordRepet = document.querySelector('#inputPasswordRepet')
+let buttonSignupRef = document.querySelector('#buttonSignup')
 let form = document.querySelector('form')
 let errorUlList = document.querySelector('.error ul')
 let errorList = document.querySelector('.error')
@@ -14,7 +16,7 @@ const errorMessage = message => {
   errorUlList.innerHTML += `<li>${message}</li>`
 }
 
-form.addEventListener('submit', event => {
+form.addEventListener('click', event => {
   errorUlList.innerHTML = ''
 
   if (empty(inputNameReference)) {
@@ -34,4 +36,33 @@ form.addEventListener('submit', event => {
     event.preventDefault()
     errorList.hidden = ''
   }
+})
+
+buttonSignupRef.addEventListener('click', event => {
+  event.preventDefault()
+
+  let user = {
+    firstName: 'string',
+    lastName: 'string',
+    email: 'string',
+    password: 'string'
+  }
+
+  let headerRequest = {
+    'Content-Type': 'application/json'
+  }
+
+  let userConfig = {
+    method: 'POST',
+    body: JSON.stringify(user),
+    headers: headerRequest
+  }
+
+  fetch('https://ctd-todo-api.herokuapp.com/v1/users', userConfig).then(
+    response => {
+      response.json().then(data => {
+        localStorage.setItem('token', data.jwt)
+      })
+    }
+  )
 })
